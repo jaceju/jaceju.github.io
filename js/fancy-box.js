7c288644ff56fe78a14a7ec7795450df7b801a1c
Jace Ju <jaceju@gmail.com>
2015-10-23 12:16:40 +0800

    Site updated: 2015-10-23 12:16:36

--- js/fancy-box.js
+++ js/fancy-box.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
-  $('.content img').each(function () {
+  $('.content img').not('.group-picture img').each(function () {
+
     var $image = $(this);
+    var imageTitle = $image.attr('title');
     var $imageWrapLink = $image.parent('a');
 
     if ($imageWrapLink.size() < 1) {
       $imageWrapLink = $image.wrap('<a href="' + this.getAttribute('src') + '"></a>').parent('a');
     }
+
     $imageWrapLink.addClass('fancybox');
+    $imageWrapLink.attr('rel', 'group');
+
+    if (imageTitle) {
+      $imageWrapLink.append('<div class="pic-title"><span>' + imageTitle + '</span></div>');
+      $imageWrapLink.attr("title", imageTitle); //make sure img title tag will show correctly in fancybox
+    }
   });
 });
+
 $('.fancybox').fancybox({
   helpers: {
     overlay: {
@@ -16,3 +26,5 @@ $('.fancybox').fancybox({
     }
   }
 });
+
+
